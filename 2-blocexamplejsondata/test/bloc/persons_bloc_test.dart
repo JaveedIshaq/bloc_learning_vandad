fb7d1d55c922d7b5b8fc0a598aad61edb561459a
javeedishaq <ishaqjaveed1@gmail.com>
2022-08-01 19:51:25 +0500

    bloc test applied

--- 2-blocexamplejsondata/test/bloc/persons_bloc_test.dart
+++ 2-blocexamplejsondata/test/bloc/persons_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:blocjsonexample/bloc/bloc_actions.dart';
 import 'package:blocjsonexample/bloc/person.dart';
 import 'package:blocjsonexample/bloc/persons_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,11 +18,14 @@ const mockedPersons2 = [
   Person(name: 'Dave2', age: 37),
 ];
 
-Future<Iterable<Person>> mockedLoader1() async => Future.value(mockedPersons1);
-Future<Iterable<Person>> mockedLoader2() async => Future.value(mockedPersons2);
+Future<Iterable<Person>> mockedLoader1(String url) =>
+    Future.value(mockedPersons1);
+Future<Iterable<Person>> mockedLoader2(String url) =>
+    Future.value(mockedPersons2);
 
 void main() {
   group('Testing Bloc', () {
+    // write tests here
     late PersonsBloc bloc;
 
     setUp(() {
@@ -34,10 +38,46 @@ void main() {
       verify: (bloc) => bloc.state == null,
     );
 
+    // fetch some mock data mockedPersons1 and compare it with FetchResult
     blocTest<PersonsBloc, FetchResult?>(
-      'Test Initial State',
+      'Mock receiving persons from first iterable',
       build: () => bloc,
-      verify: (bloc) => bloc.state == null,
+      act: (bloc) {
+        bloc.add(const LoadPersonsAction(
+          url: 'dummy_url_1',
+          loader: mockedLoader1,
+        ));
+
+        bloc.add(const LoadPersonsAction(
+          url: 'dummy_url_1',
+          loader: mockedLoader1,
+        ));
+      },
+      expect: () => [
+        const FetchResult(persons: mockedPersons1, isRetrievedFromCache: false),
+        const FetchResult(persons: mockedPersons1, isRetrievedFromCache: true)
+      ],
+    );
+
+    // fetch some mock data mockedPersons2 and compare it with FetchResult
+    blocTest<PersonsBloc, FetchResult?>(
+      'Mock receiving persons from first iterable',
+      build: () => bloc,
+      act: (bloc) {
+        bloc.add(const LoadPersonsAction(
+          url: 'dummy_url_2',
+          loader: mockedLoader2,
+        ));
+
+        bloc.add(const LoadPersonsAction(
+          url: 'dummy_url_2',
+          loader: mockedLoader2,
+        ));
+      },
+      expect: () => [
+        const FetchResult(persons: mockedPersons2, isRetrievedFromCache: false),
+        const FetchResult(persons: mockedPersons2, isRetrievedFromCache: true)
+      ],
     );
   });
 }
